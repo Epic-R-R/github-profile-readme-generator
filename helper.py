3362da2da59b3c1b1375565e652c9e1af4a6e537
Epic-R-R <epicrr001@gmail.com>
2021-05-24 11:59:38 +0430

    fix exit function

--- helper.py
+++ helper.py
@@ -1,7 +1,19 @@
 import platform
 import os
-from PyInquirer import prompt
+from PyInquirer import prompt, ValidationError, Validator
 from style import style
+import sys
+
+
+# create class for check Empty Field in input
+class EmptyValidator(Validator):
+    def validate(self, value):
+        if len(value.text):
+            return True
+        else:
+            raise ValidationError(
+                message="You can't leave this blank", cursor_position=len(value.text)
+            )
 
 def screencls():
     if platform.system() == "Linux" or platform.system() == "Darwin":
@@ -22,3 +34,15 @@ def back():
     ans = prompt(question, style=style)
     if ans["back"] == "Back":
         return True
+
+def exit():
+    questions = [
+        {
+            'type': 'confirm',
+            'message': 'Do you want to exit?',
+            'name': 'exit',
+            'default': False,
+        }
+    ]
+    answer = prompt(questions, style=style)
+    return answer['exit']
